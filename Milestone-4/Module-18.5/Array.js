e122e8fe0e95bd69b9a6989ce8c1ad6ac2682473
rakibul-islam <raakibul.uap@gmail.com>
2025-04-23 21:43:06 +0600

    array operations

---
+++ Milestone-4/Module-18.5/Array.js
@@ -0,0 +1,25 @@
+// console.log(numbers);
+
+// console.log(numbers.length);
+// console.log(numbers[0]);
+
+// let abc = 45;
+// abc = 99;
+// console.log(abc);
+// numbers[1] = 45;
+// console.log(numbers[1]);
+
+// const ages =[];
+const numbers = [1,2,3]
+console.log(numbers);
+numbers.push(44);
+numbers.push(50)
+console.log(numbers);
+
+numbers.pop()
+console.log(numbers);
+
+numbers.shift()
+console.log(numbers);
+numbers.unshift(4)
+console.log(numbers);
---
+++ Milestone-4/Module-18.5/include.js
@@ -0,0 +1,14 @@
+const numbers = [4,5,9,8,7,2]
+console.log(numbers);
+console.log(numbers.includes(4));
+
+if(numbers.includes(10)){
+    console.log("yes");
+}
+else{
+    console.log("No");
+}
+
+console.log(numbers.indexOf(2));
+console.log(numbers.indexOf(9));
+
