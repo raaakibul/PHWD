3aa05e0db5f04d42b3efffba94abc361e52c28aa
rakibul-islam <raakibul.uap@gmail.com>
2025-04-23 21:13:21 +0600

    Module added

---
+++ Milestone-4/Module-18/shortcut.js
@@ -0,0 +1,17 @@
+// const isLeader = true;
+// if(isLeader){
+//     console.log("Yes");
+// }
+// else{
+//     console.log("No");
+// }
+
+const isPassed =false;
+// logical not
+if(!isPassed){
+    console.log("Passed");
+}
+else{
+    console.log("No");
+}
+
