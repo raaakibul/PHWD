8d118d5ca979bf86b849338aae13785f6605a22d
rakibul-islam <raakibul.uap@gmail.com>
2025-04-23 21:49:57 +0600

    Loop module

---
+++ Milestone-4/Module-19/for_loop.js
@@ -0,0 +1,16 @@
+const numbers = [4,5,2,1,8,3]
+console.log(numbers);
+
+for(const number of numbers){
+    console.log(number);
+}
+
+const names= ['A','B','C','D']
+for(const name of names){
+    console.log(name);
+}
+
+const fruits = ['Mango', 'Banana', 'Orange']
+for(const fruit of fruits){
+    console.log(fruit);
+}
---
+++ Milestone-4/Module-19/while.js
@@ -0,0 +1,4 @@
+const number=0;
+while(number<5){
+    console.log("While loop");
+}
