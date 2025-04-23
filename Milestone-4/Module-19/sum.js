0f011828feeb10c824cfed6015c521d823d87286
rakibul-islam <raakibul.uap@gmail.com>
2025-04-23 22:02:18 +0600

    Loop module

---
+++ Milestone-4/Module-19/sum.js
@@ -0,0 +1,7 @@
+let num=1;
+let sum =0;
+while(num<=10){
+    sum +=num;
+    num++;
+}
+console.log(sum);
--- Milestone-4/Module-19/while.js
+++ Milestone-4/Module-19/while.js
@@ -1,4 +1,12 @@
-const number=0;
+let number=0;
 while(number<5){
     console.log("While loop");
+    number++;
 }
+// problem solving even value
+let num =0;
+while(num<100){
+    console.log(num);
+    num = num+2;
+}
+
