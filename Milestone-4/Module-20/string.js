d4ddaefa50d3c8a5c8ff786fdc6ea5bbc5f4f9e3
rakibul-islam <raakibul.uap@gmail.com>
2025-04-25 22:00:58 +0600

    string operation

--- Milestone-4/Module-20/string.js
+++ Milestone-4/Module-20/string.js
@@ -2,4 +2,36 @@ const country = 'Bangladesh';
 console.log(country);
 const s = new String("String");
 console.log(s);
+console.log(country.length);
+console.log(country[1]);
+// mutable and immutable
 
+// string comparison
+const school = 'ABC';
+console.log(school);//ABC
+
+const subject = 'Math';
+const book = 'math';
+if(subject===book){
+    console.log("Match");
+}
+else{
+    console.log("No match");// output
+}
+
+if(subject.toLocaleLowerCase()===book.toLocaleLowerCase()){
+    console.log("Match");
+}
+else{
+    console.log("No match");
+}
+
+const drink = ' water';
+const liquid = '   water';
+
+if(drink.trim()===liquid.trim()){
+    console.log("Match");
+}
+else{
+    console.log("No match");
+}
