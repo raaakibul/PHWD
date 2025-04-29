2084f28ed65bf6e6d08f2a1935edd0ec5eaeda19
rakibul-islam <raakibul.uap@gmail.com>
2025-04-29 23:26:34 +0600

    object string

---
+++ Milestone-4/Module-20/object_string.js
@@ -0,0 +1,27 @@
+const age = 21;
+const school = "ABC";
+const isPassed = true;
+let isDeveloper;
+console.log(isDeveloper);
+
+const subjects = ["Bangla", "English", "Physics", "Math"]
+console.log(subjects);
+
+// object
+const student = {
+    name: "Abc",
+    age: 21,
+    id: 123456,
+}
+console.log(student);
+console.log(student.age);
+console.log("Name : "+student.id);
+console.log(student.name);
+
+const bottle ={
+    brand: "A",
+    price: 45,
+    color: 'white',
+    isclean:false,
+}
+console.log(bottle);
