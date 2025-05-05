23d75f46f710eb2829a2354caf9402e15ede765a
rakibul-islam <raakibul.uap@gmail.com>
2025-05-05 21:51:01 +0600

    function

---
+++ Milestone-4/Module-21/function.js
@@ -0,0 +1,14 @@
+function name(){
+    return "Anis";
+}
+// call of function
+// name()
+const result = name();
+console.log(result);
+
+function sum(x,y){
+    const addition= x + y;
+    return addition;
+}
+const add = sum(5,6);
+console.log(add);
