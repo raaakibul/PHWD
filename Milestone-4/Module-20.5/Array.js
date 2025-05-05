f3911957d77e31943d85784a450b6d57ed47fb3a
rakibul-islam <raakibul.uap@gmail.com>
2025-05-05 21:20:16 +0600

    array

---
+++ Milestone-4/Module-20.5/Array.js
@@ -0,0 +1,9 @@
+const friends = ["Elon", "Bill", "Mark", "Waren"];
+for(const friend of friends){
+    console.log(friend);
+}
+
+for(let i=0;i<friends.length;i++){
+    // console.log(i);
+    console.log(i+1,friends[i]);
+}
