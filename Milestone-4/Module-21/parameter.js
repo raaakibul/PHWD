0fadca57022111bc6385c3ffb01cda945450248f
rakibul-islam <raakibul.uap@gmail.com>
2025-05-07 23:38:09 +0600

    function

---
+++ Milestone-4/Module-21/parameter.js
@@ -0,0 +1,13 @@
+function cube(x){
+    console.log(x);
+    return x*x*x;
+}
+function square(x){
+    console.log(x);
+    return x*x;
+}
+
+const result = square(5);
+const cube_result = cube(10);
+console.log(result);
+console.log(cube_result);
