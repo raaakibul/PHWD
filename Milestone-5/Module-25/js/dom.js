71dafc749ff0e1698befca3bf64babfdce8f885e
rakibul-islam <raakibul.uap@gmail.com>
2025-05-09 20:26:50 +0600

    module 25

--- Milestone-5/Module-25/js/dom.js
+++ Milestone-5/Module-25/js/dom.js
@@ -1,12 +1,25 @@
-console.log("Dom");
-console.log(document);
+// console.log("Dom");
+// console.log(document);
 const liCollection = document.getElementsByTagName('li')
-// console.log(liCollection);
+console.log(liCollection);
 for(const li of liCollection){
     console.log(li.innerText);
 }
 const allheadings = document.getElementsByTagName('h1')
-// console.log(allheadings);
+console.log(allheadings);
 for(const h1 of allheadings){
     console.log(h1);
+    console.log(h1.innerText);
+}
+
+document.getElementById('places');
+document.getElementsByClassName('important-places');
+
+const fruitsTitle = document.getElementById('fruits-title');
+fruitsTitle.innerText = 'New fruit';
+
+const places = document.getElementsByClassName('important-places');
+
+for(const place of places){
+    console.log(place.innerText);
 }
