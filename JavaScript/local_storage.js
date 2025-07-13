d59ea8e9af2dfabbbcd1f58a041903104a67fe89
rakibul-islam <raakibul.uap@gmail.com>
2025-07-13 22:29:42 +0600

    javascript local storage

---
+++ JavaScript/local_storage.js
@@ -0,0 +1,5 @@
+// localStorage.setItem("userName", "Rakibul Islam")
+// localStorage.setItem("password", "12345678")
+const userName = localStorage.getItem("userName");
+const userPassword = localStorage.getItem("password")
+console.log(userName, userPassword);
