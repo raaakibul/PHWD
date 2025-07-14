ae684e8cade488201fec1e4c3fc368ad7569bcf3
rakibul-islam <raakibul.uap@gmail.com>
2025-07-14 23:04:11 +0600

    javascript dom

---
+++ Javascript-Dom/index.js
@@ -0,0 +1,6 @@
+// getelement by id
+var heading1 = document.getElementById("heading");
+heading1.innerHTML = "Hello Dom";
+
+var paragraph = document.getElementById("para");
+paragraph.innerHTML = "Changed the paragraph text"; 
