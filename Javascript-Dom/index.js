a4be787d3f8fbcfd4618c54babe0fd05a87fbbdf
rakibul-islam <raakibul.uap@gmail.com>
2025-07-15 23:45:02 +0600

    javascript query selector

--- Javascript-Dom/index.js
+++ Javascript-Dom/index.js
@@ -4,3 +4,11 @@ heading1.innerHTML = "Hello Dom";
 
 var paragraph = document.getElementById("para");
 paragraph.innerHTML = "Changed the paragraph text"; 
+
+// Query selector
+const pid =  document.querySelector("#pid");
+pid.innerHTML = "Changed the paragraph text using query selector";
+
+document.querySelector("li a").innerHTML = "New Text";
+document.querySelector(".my-div a").innerHTML = "New Contact"
+
